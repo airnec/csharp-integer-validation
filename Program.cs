6527dee60f488469191e61e2f9f3d07cc23a7e48
Necip Dirik <necipdirik@gmail.com>
2025-01-10 10:02:35 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string? readResult = null;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10:");
+
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        int numericValue = 0;
+        validNumber = int.TryParse(readResult, out numericValue);
+
+        if (validNumber == false)
+        {
+            Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        }
+        else if (numericValue < 5 || numericValue > 10)
+        {
+            Console.WriteLine($"You entered {readResult}. Please enter a number between 5 and 10.");
+            validNumber = false;
+        }
+        else
+        {
+            Console.WriteLine($"Your input value ({readResult}) has been accepted.");
+            validNumber = true;
+        }
+    }
+} while (validNumber == false);
---
+++ README.md
@@ -0,0 +1,9 @@
+This project is solely for demonstration purposes and has been built upon what is taught on Microsoft Learn.
+
+It checks whether an integer user input is valid or not.
+
+It contains the following basics:\
+--> Nullable string type\
+--> Do-while loop\
+--> If-else if-else statement\
+--> TryParse() method
